d93cfdc205eb9f524d3b7730fa67ffa3c4d1cfe7
Marcin <94755972+Marcinzale@users.noreply.github.com>
2023-04-11 23:42:28 +0200

    Added navigations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, {useState} from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/mz-logo.png";
 
 const Navbar = () => {
-  return (
-    <div className='fixed w-full h-20 bg-blue-950 flex justify-between items-center px-4'>
-        <img src={Logo} alt="Logo" className='w-20'/>
-    </div>
-  )
-}
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
 
-export default Navbar
+  return (
+    <div className="fixed w-full h-20 bg-blue-950 flex justify-between items-center px-4 text-gray-300">
+      <img src={Logo} alt="Logo" className="w-20" />
+
+      {/* Menu */}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes/>}
+      </div>
+
+      {/* Mobile */}
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-blue-950 flex flex-col justify-center items-center'}>
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* Social icons */}
+    </div>
+  );
+};
+
+export default Navbar;
